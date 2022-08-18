2800fef5f5ff9c3b189cbd22ccc51b0186ebab05
MoosaZk <moosazkhan@gmail.com>
2022-08-19 02:38:53 +0500

    responsive Nav Bar

---
+++ js/index.js
@@ -0,0 +1,51 @@
+const body = document.querySelector('body');
+let lastScroll = 0;
+// Nav Bar Scroll Start
+
+
+window.addEventListener('scroll', () => {
+    const currentScroll = window.pageYOffset
+    if(currentScroll<= 0){
+        body.classList.remove("scrollUp");
+    }
+    if(currentScroll > lastScroll && !body.classList.contains("scrollDown")){
+        body.classList.remove("scrollUp");
+        body.classList.add("scrollDown");
+
+    }
+    if(currentScroll < lastScroll && body.classList.contains("scrollDown")){
+        body.classList.remove("scrollDown");
+        body.classList.add("scrollUp");
+
+    }
+
+    lastScroll = currentScroll;
+})
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+
+    burger.addEventListener('click', ()=> {
+            nav.classList.toggle('nav-active');
+
+            
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = '';
+            }
+            link.style.animation = 'navLinkFade 0.5s ease forwards 2s';
+        });
+
+        burger.classList.toggle("burgerMove");
+    });
+
+
+
+}
+
+navSlide();
+
+
+//Nav Bar End
